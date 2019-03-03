5366f056e14cf727826bd05b88b2d3d28eb3afc1
Gardial <dradri.khaled@gmail.com>
2019-03-03 18:37:27 +0100

    Mise en place + Test

---
+++ Deblock - 2D Isometric/Assets/CreationCube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreationCube : MonoBehaviour
+{
+    int hauteur = 10;
+    int largeur = 10;
+
+    public GameObject tuilePrefab;
+
+    // Use this for initialization
+    void Start()
+    {
+        maGrille();
+    }
+
+    void maGrille()
+    {
+        for (int i = 0; i < hauteur; i++)
+        {
+            for (int j = 0; j < largeur; j++)
+            {
+
+                GameObject tuile = Instantiate(tuilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+
+                // Nom de la tuile dans le tableau
+                tuile.name = "Tuile (" + i + "," + j + ")";
+
+            }
+        }
+    }
+}
