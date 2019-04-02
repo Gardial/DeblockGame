6b419ac3aa99f88d2b6651e51252307b2920e33a
Gardial <dradri.khaled@gmail.com>
2019-04-03 00:50:53 +0200

    Génération du SOL via Matrice OK

--- Deblock - 2D Isometric/Assets/Scenes/CreationCube.cs
+++ Deblock - 2D Isometric/Assets/Scenes/CreationCube.cs
@@ -7,10 +7,18 @@ public class CreationCube : MonoBehaviour
     int hauteur = 5; // axe Y
     int largeur = 5; // axe X
 
+    int[,] Map = new int[5, 5]
+    {
+        {2,2,2,2,2},
+        {2,1,1,1,2},
+        {2,1,1,1,2},
+        {2,1,1,1,2},
+        {2,2,2,2,2},
+    };
 
     public GameObject tuilePrefabRouge;
-    public GameObject tuilePrefabBleuDroite;
-    public GameObject tuilePrefabVertGauche;
+    public GameObject tuilePrefabBleu;
+    public GameObject tuilePrefabVerte;
  
 
     // Use this for initialization
@@ -29,12 +37,32 @@ public class CreationCube : MonoBehaviour
                 int isoX = y - x;
                 int isoY = y + x;
 
-                float valeurX = isoX * 0.64f/2; float valeurXdroite = isoX * 0.32f;
-                float valeurY = isoY * 0.64f/4; float valeurYdroite = isoY * 0.16f;
+                float valeurX = isoX * 0.64f/2; 
+                float valeurY = isoY * 0.64f/4; //ou (0.32f/2) car Y = 2*X
 
-                //Tuile Rouge
-                GameObject tuileRouge = Instantiate(tuilePrefabRouge, new Vector3(valeurX, valeurY, 0), Quaternion.identity) as GameObject;
-                tuileRouge.name = "TuileRouge (" + x + "," + y + ")"; // Nom des tuiles dans la vue hierarchie
+                int MapSwitch = Map[x, y];
+       
+
+                switch (MapSwitch)
+                {
+                    case 2:
+                        //Tuile Rouge
+                        GameObject tuileRouge = Instantiate(tuilePrefabRouge, new Vector3(valeurX, valeurY, 0), Quaternion.identity) as GameObject;
+                        tuileRouge.name = "TuileRouge (" + x + "," + y + ")"; // Nom des tuiles dans la vue hierarchie
+                        break;
+                    case 1:
+                        // Tuile Verte
+                        GameObject tuileVerte = Instantiate(tuilePrefabVerte, new Vector3(valeurX, valeurY, 0), Quaternion.identity) as GameObject;
+                        tuileVerte.name = "TuileVerte (" + x + "," + y + ")"; // Nom des tuiles dans la vue hierarchie
+                        break;
+                    default:
+                        GameObject tuileBleu = Instantiate(tuilePrefabBleu, new Vector3(valeurX, valeurY, 0), Quaternion.identity) as GameObject;
+                        tuileBleu.name = "Tuile (" + x + "," + y + ")"; // Nom des tuiles dans la vue hierarchie
+                        break;
+
+                }
+                  
+               
 
             }
         }
