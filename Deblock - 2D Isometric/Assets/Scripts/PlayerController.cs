6c848087e8e010fbc0bddacee25209a6187bff6e
Gardial <dradri.khaled@gmail.com>
2019-04-04 02:41:22 +0200

    Début gestion déplacement + position départ

--- Deblock - 2D Isometric/Assets/Scripts/PlayerController.cs
+++ Deblock - 2D Isometric/Assets/Scripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector2(0.1f, 0.1f);
+        transform.position = new Vector3(0.1f, 0.1f, -0.0001f);
         // Gestion de l'animation 
     }
 
